bcde0495817056162fedda9ce9d0cafe1fa0df34
id301 <mam.nik.al@gmail.com>
2022-08-11 02:14:21 +0300

    - add comments in add_contact - refactor login and group methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,9 +12,10 @@ class TestAddContact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_add_contact_page(wd)
-        self.add_contact(wd)
+        self.add_contact(wd, "John", "Doe", "City, random street, 1 building", "3417698", "+79342319032",
+                         "test@test.ru")
         self.return_to_contacts_page(wd)
         self.logout(wd)
 
@@ -24,37 +25,43 @@ class TestAddContact(unittest.TestCase):
     def return_to_contacts_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def add_contact(self, wd):
+    def add_contact(self, wd, firstname, lastname, address, home_number, phone_number, email):
+        ##add firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("John")
+        wd.find_element_by_name("firstname").send_keys(firstname)
+        #add lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Doe")
+        wd.find_element_by_name("lastname").send_keys(lastname)
+        #add address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("City, random street, 1 building")
+        wd.find_element_by_name("address").send_keys(address)
+        #add phone numbers
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("3417698")
+        wd.find_element_by_name("home").send_keys(home_number)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+79342319032")
+        wd.find_element_by_name("mobile").send_keys("%s" % phone_number)
+        #add email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("test@test.ru")
+        wd.find_element_by_name("email").send_keys("%s" % email)
+        #save
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def open_add_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):

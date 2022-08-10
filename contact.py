0a01e260c61f9c8970b332aaed1de07da9bcd9db
id301 <mam.nik.al@gmail.com>
2022-08-11 02:21:22 +0300

    added support class Contact

---
+++ contact.py
@@ -0,0 +1,11 @@
+__author__ = 'id301'
+
+class Contact:
+
+    def __init__(self, firstname, lastname, address, home_number, phone_number, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home_number = home_number
+        self.phone_number = phone_number
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -2,22 +2,14 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from contact import Contact
 import unittest, time, re
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-    
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_contact_page(wd)
-        self.add_contact(wd, "John", "Doe", "City, random street, 1 building", "3417698", "+79342319032",
-                         "test@test.ru")
-        self.return_to_contacts_page(wd)
-        self.logout(wd)
+
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
@@ -25,30 +17,30 @@ class TestAddContact(unittest.TestCase):
     def return_to_contacts_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def add_contact(self, wd, firstname, lastname, address, home_number, phone_number, email):
+    def add_contact(self, wd, contact):
         ##add firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         #add lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         #add address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         #add phone numbers
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home_number)
+        wd.find_element_by_name("home").send_keys(contact.home_number)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("%s" % phone_number)
+        wd.find_element_by_name("mobile").send_keys("%s" % contact.phone_number)
         #add email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("%s" % email)
+        wd.find_element_by_name("email").send_keys("%s" % contact.email)
         #save
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -66,6 +58,18 @@ class TestAddContact(unittest.TestCase):
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_contact_page(wd)
+        self.add_contact(wd, Contact("John", "Doe", "City, random street, 1 building", "3417698", "+79342319032",
+                         "test@test.ru"))
+        self.return_to_contacts_page(wd)
+        self.logout(wd)
+
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)

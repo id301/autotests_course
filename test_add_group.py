c74bc0a0eba9f54f45d1554f580560d668d77dee
id301 <mam.nik.al@gmail.com>
2022-08-14 19:46:07 +0300

    open page actions moved to auxilary methods

--- test_add_group.py
+++ test_add_group.py
@@ -20,6 +20,7 @@ class test_add_group(unittest.TestCase):
 
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -34,6 +35,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         #submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
 
     def open_groups_page(self, wd):
@@ -41,6 +43,7 @@ class test_add_group(unittest.TestCase):
 
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -55,20 +58,14 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="dsfdfs", header="dfsdf", footer="dfdsfdf"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):

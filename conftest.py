81188968819d7d5cacff15e93c90dd1b5b63fa7d
id301 <mam.nik.al@gmail.com>
2022-08-17 09:43:40 +0300

    added conftest

---
+++ conftest.py
@@ -0,0 +1,11 @@
+__author__ = 'id301'
+
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture

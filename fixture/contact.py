56c1b4783e2a86d21708267b186446e24cf411b6
id301 <mam.nik.al@gmail.com>
2022-08-17 09:21:35 +0300

    added auxilary class ContactHelper

---
+++ fixture/contact.py
@@ -0,0 +1,42 @@
+__author__ = 'id301'
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_contacts_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def add(self, contact):
+        wd = self.app.wd
+        ##add firstname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        #add lastname
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        #add address
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        #add phone numbers
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_number)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("%s" % contact.phone_number)
+        #add email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("%s" % contact.email)
+        #save
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def open_add_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()

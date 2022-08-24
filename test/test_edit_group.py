296fc7961d889f2963093adf79d6374626327e68
id301 <mam.nik.al@gmail.com>
2022-08-24 07:13:47 +0300

    add preconditions to test del and edit group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,4 +3,6 @@ __author__ = 'id301'
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Not edited", header="Not edited", footer="Not edited"))
     app.group.edit_first(Group(name="Edited", header="Edited", footer="Edited"))

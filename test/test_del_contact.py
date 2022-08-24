e4ba8323381281aa09066b3f81f8714710a7c992
id301 <mam.nik.al@gmail.com>
2022-08-24 07:27:40 +0300

    add preconditions to test del and edit contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,8 @@
 __author__ = 'id301'
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add(Contact("John", "Doe", "City, random street, 1 building", "3417698", "+79342319032",
+                                "test@test.ru"))
     app.contact.delete_first()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,5 +3,8 @@ __author__ = 'id301'
 from model.contact import Contact
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add(Contact("John", "Doe", "City, random street, 1 building", "3417698", "+79342319032",
+                                "test@test.ru"))
     app.contact.edit_first(Contact("Edited name", "Edited surname", "Edited City, random street, 1 building", "000000", "+79999999999",
                 "edited@test.ru"))

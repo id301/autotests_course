6fb325f78b54bccbbad3c1e6744a0386ccfb378e
id301 <mam.nik.al@gmail.com>
2022-09-11 22:24:40 +0300

    add load testdata from files for contacts

--- data/contacts.py
+++ data/contacts.py
@@ -1,8 +1,6 @@
 from model.contact import Contact
-import random
-import string
 
-constant = [
+testdata = [
     Contact(firstname="firstname1", lastname="lastname1", address="address1", home_number="0000000", work_phone="2222222", phone_number="+79999999999",
             secondary_phone="4444444", email="mail1@test.ru", email2="mail1@test.com", email3="mail1@test.net"),
     Contact(firstname="firstname2", lastname="lastname2", address="address2", home_number="1111111", work_phone="3333333", phone_number="+78888888888",
--- generator/contact.py
+++ generator/contact.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
+import jsonpickle
 from model.contact import Contact
 import random
 import string
@@ -50,4 +50,5 @@ testdata = [Contact(firstname="", lastname="", address="", home_number="", work_
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))

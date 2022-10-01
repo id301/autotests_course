70ec21a036223faf92ee0e1bf6a3a36bf8ace3cc
id301 <mam.nik.al@gmail.com>
2022-10-01 18:52:15 +0300

    -modified group tests for using db connection -added option on list check

--- fixture/group.py
+++ fixture/group.py
@@ -80,6 +80,19 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        #open modification form
+        wd.find_element_by_name("edit").click()
+        #fill group form
+        self.fill_group_form(new_group_data)
+        #submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def modify_first_group(self, new_group_data):
         self.modify_group_by_index(0, new_group_data)
 
@@ -92,10 +105,10 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def delete_by_id(self, index):
+    def delete_by_id(self, id):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_group_by_id(index)
+        self.select_group_by_id(id)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -10,8 +10,6 @@ def test_delete_some_group(app, db, check_ui):
     group = random.choice(old_groups)
     app.group.delete_by_id(group.id)
     new_groups = db.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
-    assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
